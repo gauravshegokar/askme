0a5e4f80bb6170aec7eee74fb98b8c090f50cc82
Gongpu Zhu <ZhuGongpu@users.noreply.github.com>
2020-03-18 20:43:28 -0700

    server: add Comment model

--- server/app/models/Comment.java
+++ server/app/models/Comment.java
@@ -2,6 +2,54 @@ package models;
 
 
 import com.avaje.ebean.Model;
+import play.data.validation.Constraints;
+
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import java.util.Date;
 
 public class Comment extends Model {
+    @Id
+    private long commendId;
+
+    @Constraints.Required
+    private String commentText;
+
+    @Constraints.Required
+    private Date date;
+
+    @OneToOne
+    private UserProfile user;
+
+    public long getCommendId() {
+        return commendId;
+    }
+
+    public void setCommendId(long commendId) {
+        this.commendId = commendId;
+    }
+
+    public String getCommentText() {
+        return commentText;
+    }
+
+    public void setCommentText(String commentText) {
+        this.commentText = commentText;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public UserProfile getUser() {
+        return user;
+    }
+
+    public void setUser(UserProfile user) {
+        this.user = user;
+    }
 }
