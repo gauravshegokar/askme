a49e0135d2c334279c6c711e21909443b088a123
Gaurav Shegokar <gaurao2009@gmail.com>
2020-03-25 19:15:34 -0700

    allowing logged in user to view other users profiles

--- server/app/controllers/UserInformationController.java
+++ server/app/controllers/UserInformationController.java
@@ -9,12 +9,13 @@ import static play.mvc.Http.Context.Implicit.request;
 public class UserInformationController extends Controller {
     public Result getUserBasicInfo(String userId) {
 
-
-        if (request().headers().get("auth")[0].equals(userId)) {
-            return new UserInformationService().getUserBasicInfo(userId);
-        } else {
-            return badRequest("\"{\"error\":\"Authentication failed, Please login again\"}\"");
-        }
+        // any user can check any users profile
+        return new UserInformationService().getUserBasicInfo(userId);
+//        if (request().headers().get("auth")[0].equals(userId)) {
+//            return new UserInformationService().getUserBasicInfo(userId);
+//        } else {
+//            return badRequest("\"{\"error\":\"Authentication failed, Please login again\"}\"");
+//        }
     }
 
     //TODO: complete it in later sprints
@@ -24,11 +25,11 @@ public class UserInformationController extends Controller {
 
     public Result getUserPosts(String userId) {
 
-        boolean isAuthentic = request().headers().get("auth")[0].equals(userId) ? true : false;
-        if (!isAuthentic) {
-            return badRequest("\"{\"error\":\"Authentication failed, Please login again\"}\"");
-        }
-
+//        boolean isAuthentic = request().headers().get("auth")[0].equals(userId) ? true : false;
+//        if (!isAuthentic) {
+//            return badRequest("\"{\"error\":\"Authentication failed, Please login again\"}\"");
+//        }
+        // any user can check any users profile
         return new UserInformationService().getUserPosts(userId);
 
     }
@@ -36,11 +37,11 @@ public class UserInformationController extends Controller {
     //TODO: by default returning channel with id 1, change it in future sprints
     public Result getUserSubscribedChannels(String userId){
 
-        boolean isAuthentic = request().headers().get("auth")[0].equals(userId) ? true : false;
-        if (!isAuthentic) {
-            return badRequest("\"{\"error\":\"Authentication failed, Please login again\"}\"");
-        }
-
+//        boolean isAuthentic = request().headers().get("auth")[0].equals(userId) ? true : false;
+//        if (!isAuthentic) {
+//            return badRequest("\"{\"error\":\"Authentication failed, Please login again\"}\"");
+//        }
+        // any user can check any users profile
         return  new UserInformationService().getUserSubscribedChannels(userId);
     }
 }
