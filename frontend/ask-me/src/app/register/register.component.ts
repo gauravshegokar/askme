d54cada260b9b5064eb15465490113d19207762c
Gaurav Shegokar <gaurao2009@gmail.com>
2020-03-26 21:14:01 -0700

    memento design pattern implemented

--- frontend/ask-me/src/app/register/register.component.ts
+++ frontend/ask-me/src/app/register/register.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { RegisterService } from './register.service'
 import { Router } from '@angular/router';
+import { Caretaker, Originator } from './register-memento'
+import { ClonerService } from '../_services/cloner.service'
 
 @Component({
   selector: 'app-register',
@@ -10,22 +12,62 @@ import { Router } from '@angular/router';
 })
 export class RegisterComponent implements OnInit {
   submitted = false;
-  form: FormGroup = new FormGroup({
-    username: new FormControl('', [Validators.required, Validators.minLength(4)]),
-    password: new FormControl('', [Validators.required, Validators.minLength(4)]),
-    fname: new FormControl(''),
-    lname: new FormControl(''),
-    userType: new FormControl('regular'),
-  });
+  form: FormGroup;
+
+  private originator: Originator
+  private caretaker: Caretaker
 
   error: string | null;
 
   constructor(private registerService: RegisterService,
-    private router: Router
+    private router: Router,
+    private clonerService: ClonerService
   ) {
   }
 
   ngOnInit(): void {
+    this.initialize()
+  }
+
+  initialize() {
+
+    // saving 
+    this.form = this.getFormInstance()
+
+    this.caretaker = new Caretaker();
+    this.originator = new Originator();
+
+    this.originator.setState(this.clonerService.deepClone(this.form))
+    this.caretaker.addMemento(this.originator.commit())
+
+  }
+
+  getFormInstance() {
+    return new FormGroup({
+      username: new FormControl('', [Validators.required, Validators.minLength(4)]),
+      password: new FormControl('', [Validators.required, Validators.minLength(4)]),
+      fname: new FormControl(''),
+      lname: new FormControl(''),
+      userType: new FormControl('regular'),
+    });
+  }
+
+  resetForm() {
+    console.log("resetting the form")
+    this.form = this.clonerService.deepClone(this.caretaker.getMemento(0).getState())
+  }
+
+  saveForm() {
+    console.log("saving the form")
+    // let currentSnapshot = Object.assign({}, this.form)
+    let currentSnapshot = this.clonerService.deepClone(this.form)
+    this.originator.setState(currentSnapshot)
+    this.caretaker.addMemento(this.originator.commit())
+  }
+
+  undoForm() {
+    console.log("undo the form")
+    this.form = this.clonerService.deepClone(this.caretaker.getMemento(-1).getState())
   }
 
   register() {
